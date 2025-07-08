28010fdc1f511b9e7d7fea79333fc9b92a6fafec
dmnavushtanov <dmnavushtanov@abv.bg>
2025-07-08 17:33:25 +0300

    fix typing and remove line clamp

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,12 +1,13 @@
 import { getEventBySlug, getEvents } from '@/lib/content';
 import EventDetailsClient from '@/components/EventDetailsClient';
+import type { PageProps } from 'next';
 
 export async function generateStaticParams() {
   const events = await getEvents('bg');
   return events.map((event) => ({ slug: event.slug }));
 }
 
-export default async function EventPage({ params }: { params: { slug: string } }) {
+export default async function EventPage({ params }: PageProps<{ slug: string }>) {
   const decodedSlug = decodeURIComponent(params.slug);
   const event = await getEventBySlug(decodedSlug, 'bg');
 
